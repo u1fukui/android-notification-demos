f254070851aa030828b4b8b2f0a4d33ee88a8c20
u1 <u1.kobayashi@gmail.com>
2016-10-21 21:33:19 +0900

    Add spinner that select notification style.

--- app/src/main/java/com/u1fukui/android/demo/notification/MainActivity.java
+++ app/src/main/java/com/u1fukui/android/demo/notification/MainActivity.java
@@ -12,9 +12,20 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements ClickEventHandler {
+
+    private static final String STYLE_NO = "NoStyle";
+
+    private static final String STYLE_BIG_TEXT = "BigTextStyle";
+
+    private static final String STYLE_BIG_PICTURE = "BigPictureStyle";
+
+    private static final String STYLE_MEDIA = "MediaStyle";
+
+    private static final String STYLE_MESSAGING = "MessagingStyle";
 
     private MainActivityBinding binding;
 
@@ -23,6 +34,22 @@ public class MainActivity extends AppCompatActivity implements ClickEventHandler
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.main_activity);
         binding.setHandler(this);
+
+        initSpinner();
+        binding.notificationIdEditText.setSelection(binding.notificationIdEditText.getText().length());
+    }
+
+    private void initSpinner() {
+        String[] array = {
+                STYLE_NO,
+                STYLE_BIG_TEXT,
+                STYLE_BIG_PICTURE,
+                STYLE_MEDIA,
+                STYLE_MESSAGING
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item_style);
+        adapter.addAll(array);
+        binding.spinner.setAdapter(adapter);
     }
 
     @Override
@@ -45,6 +72,23 @@ public class MainActivity extends AppCompatActivity implements ClickEventHandler
     }
 
     private void showNotification(int notificationId) {
+        String item = (String) binding.spinner.getSelectedItem();
+        switch (item) {
+            case STYLE_NO:
+                showDefaultNotification(notificationId);
+                break;
+            case STYLE_BIG_TEXT:
+                break;
+            case STYLE_BIG_PICTURE:
+                break;
+            case  STYLE_MEDIA:
+                break;
+            case STYLE_MESSAGING:
+                break;
+        }
+    }
+
+    private void showDefaultNotification(int notificationId) {
         NotificationCompat.Builder builder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_launcher)
